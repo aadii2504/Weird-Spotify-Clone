90d937633fc4765f4abd34c6bbad70a662ad67e9
aadii2504 <119524954+aadii2504@users.noreply.github.com>
2024-02-04 12:32:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder){
     currFolder = folder;
-    let  a = await fetch(`http://127.0.0.1:5500/${folder}/`);
+    let  a = await fetch(`https://aadii2504.github.io/Weird-Spotify-Clone/${folder}/`);
     let response  = await a.text();
     // console.log(response);
     let div  =  document.createElement("div")
@@ -71,7 +71,7 @@ const playMusic  = (track , pause = false)=> {
 }
 
 async function displayAlbums(){
-    let  a = await fetch(`http://127.0.0.1:5500/songs/`);
+    let  a = await fetch(`https://aadii2504.github.io/Weird-Spotify-Clone/songs/`);
     let response  = await a.text();
     let div  =  document.createElement("div")
     div.innerHTML = response;
@@ -83,7 +83,7 @@ async function displayAlbums(){
         if(e.href.includes("/songs/")){
             let folder = e.href.split("/").slice(-1)[0]
             //get the metadata of the folder 
-            let  a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+            let  a = await fetch(`https://aadii2504.github.io/Weird-Spotify-Clone/songs/${folder}/info.json`);
             let response  = await a.json();
             // console.log(response)
             cardContainer.innerHTML  = cardContainer.innerHTML + `  <div data-folder="${folder}" class="card">
